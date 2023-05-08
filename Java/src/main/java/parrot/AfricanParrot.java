15125aaad2e68aac06c3a7549bc88d380943ef54
Martin Nitsch <martin.nitsch@andrena.de>
2023-05-08 17:27:42 +0200

    Add possible solution

---
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,21 @@
+package parrot;
+
+public class AfricanParrot extends Parrot{
+
+
+    private final int numberOfCoconuts;
+
+    public AfricanParrot(int numberOfCoconuts) {
+        super();
+        this.numberOfCoconuts = numberOfCoconuts;
+    }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
+    private double getLoadFactor() {
+        return 9.0;
+    }
+}
---
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,21 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot{
+
+    private final double voltage;
+    private final boolean isNailed;
+    public NorwegianBlueParrot(double voltage, boolean isNailed) {
+        super();
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+
+    @Override
+    public double getSpeed() {
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
+    }
+
+    private double getBaseSpeed(double voltage) {
+        return Math.min(24.0, voltage * getBaseSpeed());
+    }
+}
